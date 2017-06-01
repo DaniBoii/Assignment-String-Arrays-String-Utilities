a1edea603ed6eb537f56179525b1df8af600215c
M0neyKick <khalid.ozal26@gmail.com>
2017-06-01 12:48:04 -0400

    Update String & Arrays - String Utilities.java

--- String & Arrays - String Utilities.java
+++ String & Arrays - String Utilities.java
@@ -1,16 +1,6 @@
 //String & Arrays - String Utilities
 class stringarraysstringutilities
 {
-  public static void main (String[] args){
-    System.out.println("Please enter you first name");
-    String userName = "";
-    userName = In.getString();
-    System.out.println( userName + " Welcome to Subprograms Menu");
-// User picks what they want to do
-    subAssignment();
-  }
-  
-  
   
   public static void subAssignment()   
   {
@@ -67,8 +57,8 @@ class stringarraysstringutilities
     search = letterK  +" - " + search;
     searchAndReplace = letterL  +" - " + searchAndReplace;
     System.out.println(countLetters + "(This sub program will count the amount the letters you have entered)");
-    System.out.println(countWord);
-    System.out.println(capEveryWord);
+    System.out.println(countWord + "(This sub program will count the amount the words you have entered)");
+    System.out.println(capEveryWord +"(This sub program will Capitalize every word you have entered)" );
     System.out.println(revereStatement);
     System.out.println(revereLetters);
     System.out.println(reverseWord);
@@ -78,9 +68,109 @@ class stringarraysstringutilities
     System.out.println(lettersOnly);
     System.out.println(search);
     System.out.println(searchAndReplace);
-    
-    
-    
+  } 
+  
+  
+  
+  
+  public static void doLetterSS(String letters)
+//INPUT FROM USER
+  {
+    System.out.print("Please enter a statement");
+    letters = In.getString (); 
+    String str = letters;
+    int counter = 0;
+    {
+//COUNTING LETTERS     
+      for (int i = 0; i < str.length(); i++)
+      {
+        if (Character.isLetter(str.charAt(i)))
+          counter++;
+      }
+//OUTPUT FRO PROGRAM
+      System.out.println(counter + " letters.");
+    }
   }
   
+  
+// Reverse  
+  public static void reverse(String statement){
+    System.out.println("Please enter a statement?");
+    statement = In.getString();
+    String result="";
+    for(int i = statement.length()-1; i<=0; i--) {
+      result = result + statement.charAt(i);
+    }
+    System.out.println(result);
+  }
+  
+  
+// Cap words method  
+public static void capWord(String statement)
+{
+    
+    System.out.println("Please enter your statement?");
+    statement = In.getString();
+    
+    String capitalize = null;
+    for(int i=0; i< (statement.length());i++){
+      
+      if(statement.charAt(i)==' '){
+        
+        capitalize= capitalize.substring(0,i+1)+statement.substring(i+1,i+2).toUpperCase()+statement.substring(i+2);
+        
+      }else if(i==0){
+        
+        capitalize=statement.substring(0,1).toUpperCase()+statement.substring(1);
+        
+      }
+    }
+    System.out.println("statement: "+capitalize);
+  }
+  
+  
+// Main code  
+  public static void main (String[] args){
+    String letters= "";
+    String statement = "";
+
+    System.out.print("Please enter you first name");
+    String userName = "";
+    userName = In.getString();
+    System.out.println();
+    System.out.println( userName + " Welcome to Subprograms Menu");
+    System.out.println();
+// Dispapling options
+    subAssignment();
+// Getting what program to run
+    String runProgram = "";
+    System.out.print("Please enter a letter between A to L");
+    runProgram = In.getString();
+    System.out.println();
+    if(runProgram.equals ("A") || runProgram.equals ("a"))
+    {
+      doLetterSS(letters);
+    }
+    if(runProgram.equals ("B") || runProgram.equals ("b"))
+    {
+      //Count word
+    }
+    if(runProgram.equals ("B") || runProgram.equals ("b"))
+    {
+      
+    }
+    if(runProgram.equals ("C") || runProgram.equals ("c"))
+    {
+      capWord(statement);
+    }
+    if(runProgram.equals ("D") || runProgram.equals ("d"))
+    {
+      
+    }
+    if(runProgram.equals ("E") || runProgram.equals ("e"))
+    {
+      reverse(statement);
+    }
+    
+  }
 }
